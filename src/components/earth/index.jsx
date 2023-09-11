01e32ee4fa625ab8c8e71ebd46d6276699810f6e
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-11 13:53:10 +0200

    touching the stars

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import * as THREE from "three";
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures//8k_earth_clouds.jpg";
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
+import { OrbitControls, Stars } from "@react-three/drei";
 
 export function Earth(props) {
   const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
@@ -14,11 +16,43 @@ export function Earth(props) {
 
   return (
     <>
-      <ambientLight intensity={1} />
+      <ambientLight intensity={1.5} />
+      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={1.2} />
+      <Stars
+        radius={300}
+        depth={60}
+        count={20000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
+      <mesh>
+        <sphereGeometry args={[1.005, 32, 32]} />
+        <meshPhongMaterial
+          map={cloudsMap}
+          opacity={0.4}
+          depthWrite={true}
+          transparent={true}
+          side={THREE.DoubleSide}
+        />
+      </mesh>
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
-        <meshPhongMaterial color="red" />
-        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
+        <meshPhongMaterial specularMap={specularMap} />
+        <meshStandardMaterial
+          map={colorMap}
+          normalMap={normalMap}
+          metalness={0.4}
+          roughness={0.7}
+        />
+        <OrbitControls
+          enableZoom={true}
+          enablePan={true}
+          enableRotate={true}
+          zoomSpeed={0.6}
+          panSpeed={0.5}
+          rotateSpeed={0.4}
+        />
       </mesh>
     </>
   );
