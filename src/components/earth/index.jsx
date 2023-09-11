5098b90a62252ecb285fd22b919587034ba9ab74
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-09-11 12:58:40 +0200

    Adding Texture

--- src/components/earth/index.jsx
+++ src/components/earth/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg";
+import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
+import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
+import EarthCloudsMap from "../../assets/textures//8k_earth_clouds.jpg";
+import { useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
 
 export function Earth(props) {
+  const [colorMap, normalMap, specularMap, cloudsMap] = useLoader(
+    TextureLoader,
+    [EarthDayMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
+  );
+
   return (
     <>
-      <ambientLight />
+      <ambientLight intensity={1} />
       <mesh>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial color="red" />
+        <meshStandardMaterial map={colorMap} normalMap={normalMap} />
       </mesh>
     </>
   );
